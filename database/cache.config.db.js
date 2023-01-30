102beff677dce5df1565617de3a1d80db8c54a22
ANGERSON JOSE AMUNDARAY RENGIFO <amundaray24@gmail.com>
2023-01-30 19:34:10 +0100

    [NODE] Fixing Docker Redis Connection

--- database/cache.config.db.js
+++ database/cache.config.db.js
@@ -7,12 +7,17 @@ const {
 } = process.env;
 
 const cacheClient = redis.createClient({
-  host: IMAGES_TMP_CACHE_HOST,
-  port: IMAGES_TMP_CACHE_PORT,
+  url: `redis://${IMAGES_TMP_CACHE_HOST}:${IMAGES_TMP_CACHE_PORT}`,
   password: IMAGES_TMP_CACHE_PASSWORD
 });
 
+console.log(`CONNECTION - HOST: ${IMAGES_TMP_CACHE_HOST}, PORT: ${IMAGES_TMP_CACHE_PORT}`);
+
 cacheClient.connect();
+
+cacheClient.on('error', (err) =>  {
+  console.log('ERROR - Cache error',err);
+});
 
 cacheClient.on('connect', () =>  {
   console.log('INFO - Cache Connected');
